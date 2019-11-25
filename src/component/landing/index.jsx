e199293906f008c48146c5341878571cff8e6a35
okoroemeka <solomonokoro056@gmail.com>
2019-11-25 09:59:06 +0100

    ft(landing page): Landing page
    - Add landing page markup
    
    - Add landing page style
    
    [Delivers #169940739]

---
+++ src/component/landing/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from '@reach/router';
+import './landing.scss';
+
+class LandingPage extends Component {
+  state = {};
+
+  render() {
+    const { authLogin } = this.props;
+    if (authLogin.isLoggedIn) return <Redirect noThrow to="/dashboard" />;
+    return (
+      <div className="landing__page__container">
+        <div className="landing__page__overlay">
+          <div className="details__area">
+            <div className="title__container">
+              <h1>Lender</h1>
+              <h3>Never go out dry, we are here to help. </h3>
+            </div>
+            <div className="button__container">
+              <Link to="/login" className="login__button">
+                Log in
+              </Link>
+              <Link to="/signup" className="signup__button">
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  authLogin: state.authLogin,
+});
+export default connect(
+  mapStateToProps,
+  {},
+)(LandingPage);
+// export default LandingPage;
